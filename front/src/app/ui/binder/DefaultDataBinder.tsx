e3b16c26d7f9f5c11202fd9ef89d4819fc747fc3
anasoid <oufdou.anas@gmail.com>
2024-08-14 22:24:45 +0100

    fix obecttype

--- front/src/app/ui/binder/DefaultDataBinder.tsx
+++ front/src/app/ui/binder/DefaultDataBinder.tsx
@@ -1,8 +1,7 @@
 import { InputNumberValueChangeEvent } from 'primereact/inputnumber';
 import { Dispatch, SetStateAction } from 'react';
-import { ObjectType } from 'typescript';
 
-export class DefaultDataBinder<T extends ObjectType> {
+export class DefaultDataBinder<T> {
     setItem: Dispatch<SetStateAction<T>>;
     item: T;
 
@@ -10,19 +9,19 @@ export class DefaultDataBinder<T extends ObjectType> {
         this.setItem = setItem;
         this.item = item;
     }
-    onInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, name: string) => {
+    onInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, name: keyof T) => {
         const val = (e.target && e.target.value) || '';
         this.updateValue(val, name);
     };
 
-    onInputNumberChange = (e: InputNumberValueChangeEvent, name: string) => {
+    onInputNumberChange = (e: InputNumberValueChangeEvent, name: keyof T) => {
         const val = e.value || 0;
         this.updateValue(val, name);
     };
 
-    updateValue(val: any, name: string) {
+    updateValue(val: any, name: keyof T) {
         let _item = { ...this.item };
-        _item[`${name}`] = val;
+        _item[name] = val;
         this.setItem(_item);
     }
 }
