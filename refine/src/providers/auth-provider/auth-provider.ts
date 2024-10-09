747806c998dfb49209ff42e4f79745fa15194bec
anas <oufdou.anas@gmail.com>
2024-10-09 13:31:58 +0100

    clean log

--- refine/src/providers/auth-provider/auth-provider.ts
+++ refine/src/providers/auth-provider/auth-provider.ts
@@ -7,7 +7,6 @@ import { authManager } from "./AuthManager";
 export const authProvider: AuthProvider = {
   login: async ({ email, username, password, remember }) => {
     // Suppose we actually send a request to the back end here.
-    console.log("login");
     let result: boolean = await authManager.login(email, password);
 
     if (result) {
@@ -26,7 +25,6 @@ export const authProvider: AuthProvider = {
     };
   },
   logout: async () => {
-    console.log("logout");
     authManager.logout();
     return {
       success: true,
@@ -34,7 +32,6 @@ export const authProvider: AuthProvider = {
     };
   },
   check: async () => {
-    console.log("check");
     const auth = authManager.isAuthenticated();
     if (auth) {
       return {
@@ -49,15 +46,12 @@ export const authProvider: AuthProvider = {
     };
   },
   getPermissions: async () => {
-    console.log("getPermissions");
     return authManager.getCurrentRoles();
   },
   getIdentity: async () => {
-    console.log("getIdentity");
     return authManager.getCurrentUser();
   },
   onError: async (error) => {
-    console.log("onError");
     if (error.response?.status === 401) {
       return {
         logout: true,
